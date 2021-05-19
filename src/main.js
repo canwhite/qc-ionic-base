559ba2fc466e78d4ddc254881ce8f275cabfa1aa
qiaochao <752716886@qq.com>
2021-05-19 18:03:20 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 //IonicVue该插件是一个插件，可以让我们在Vue环境中使用Ionic Framework。
-import { IonicVue } from '@ionic/vue';
+// import { IonicVue } from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
